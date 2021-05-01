7f78fb329b6dd77fc0458cbfd3c62a31f9eed0e5
izanagiwarrior <m.faiztpaduhai@gmail.com>
2021-05-02 05:26:11 +0700

    rapiin routing

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -8,21 +8,90 @@ use Illuminate\Support\Facades\Storage;
 
 class MyController extends Controller
 {
+    public function landing()
+    {
+        return view('landing');
+    }
+
+    public function about_us()
+    {
+        return view('about_us');
+    }
+
+    public function login_view()
+    {
+        return view('login');
+    }
+
+
+    public function register_view()
+    {
+        return view('register');
+    }
+
+    public function keranjang()
+    {
+        return view('keranjang');
+    }
+
+    public function barat()
+    {
+        return view('barat');
+    }
+
+    public function utara()
+    {
+        return view('utara');
+    }
+
+    public function selatan()
+    {
+        return view('selatan');
+    }
+
+    public function profile()
+    {
+        return view('profile');
+    }
+
+    public function admin_reg()
+    {
+        return view('register_admin'); //Anomali
+    }
+
+    public function tambah_produk_view()
+    {
+        return view('tambah_produk');
+    }
+
+    public function hapus_edit_produk()
+    {
+        return view('hapus_edit_produk');
+    }
+
+    public function search()
+    {
+        return view('search');
+    }
+
+
+    // ===========================================================================================================================
+    // ======================================================== POST AREA ========================================================
+    // ===========================================================================================================================
+
+
     public function register(Request $request)
     {
         session()->regenerate();
         if ($request->input("password") != $request->input("re-password"))
             return redirect("register?message=Konfirmasi Password Salah!");
-        DB::insert('insert into akun (username,nama_lengkap,password,email,nohp,alamat,jenis_kelamin,tanggal_lahir,role,csrf) 
-        values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [$request->input("username"),$request->input("username"),$request->input("password"),$request->input("email"),$request->input("nohp"),$request->input("alamat"),"laki-laki","2020-10-10 10:10:10","user",""]);
-        return redirect("register?message=Berhasil Register");
-    }
+        DB::insert(
+            'insert into akun (username,nama_lengkap,password,email,nohp,alamat,jenis_kelamin,tanggal_lahir,role,csrf) 
+            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [$request->input("username"), $request->input("username"), $request->input("password"), $request->input("email"), $request->input("nohp"), $request->input("alamat"), "laki-laki", "2020-10-10 10:10:10", "user", ""]
+        );
 
-    public function logout(Request $request)
-    {
-        session()->regenerate();
-        return redirect("login");
+        return redirect("register?message=Berhasil Register");
     }
 
     public function register_admin(Request $request)
@@ -30,9 +99,12 @@ class MyController extends Controller
         session()->regenerate();
         if ($request->input("password") != $request->input("re-password"))
             return redirect("register?message=Konfirmasi Password Salah!");
-        DB::insert('insert into akun (username,nama_lengkap,password,email,nohp,alamat,jenis_kelamin,tanggal_lahir,role,csrf) 
-        values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [$request->input("username"),$request->input("username"),$request->input("password"),$request->input("email"),$request->input("nohp"),$request->input("alamat"),"laki-laki","2020-10-10 10:10:10","admin",""]);
+        DB::insert(
+            'insert into akun (username,nama_lengkap,password,email,nohp,alamat,jenis_kelamin,tanggal_lahir,role,csrf) 
+            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [$request->input("username"), $request->input("username"), $request->input("password"), $request->input("email"), $request->input("nohp"), $request->input("alamat"), "laki-laki", "2020-10-10 10:10:10", "admin", ""]
+        );
+
         return redirect("register?message=Berhasil Register");
     }
 
@@ -40,29 +112,25 @@ class MyController extends Controller
     {
         session()->regenerate();
         $result = DB::select('select id,password from akun where username = ?', [$request->input("username")]);
-        if ($result == null){
+        if ($result == null) {
             return redirect("/login?message=Username tidak ditemukan");
         }
-        if ($result[0]->password == $request->input("password"))
-        {
-            DB::update('update akun set csrf = ? where id = ?', [csrf_token(),$result[0]->id]);
-        }else
-        {
+        if ($result[0]->password == $request->input("password")) {
+            DB::update('update akun set csrf = ? where id = ?', [csrf_token(), $result[0]->id]);
+        } else {
             return redirect("/login?message=Password Salah");
         }
+
         return redirect("/");
     }
-    
+
     public function edit_profile(Request $request)
     {
-        if ($request->input("password") != $request->input("passwordc"))
-        {
+        if ($request->input("password") != $request->input("passwordc")) {
             return redirect("/profile?message=Salah Password");
         }
-		
-		
+        DB::update('update akun set nama_lengkap = ?, nohp = ? where csrf = ?', [$request->input("namadd"), $request->input("nohp"), csrf_token()]);
 
-        DB::update('update akun set nama_lengkap = ?, nohp = ? where csrf = ?', [$request->input("namadd"),$request->input("nohp"),csrf_token()]);
         return redirect("/profile");
     }
 
@@ -72,28 +140,31 @@ class MyController extends Controller
             return redirect("/login");
         }
         $exist = DB::select('select * from keranjang where id_produk = ?', [$request->input("id_produk")]);
-        if ($exist!=null)
-        {
+        if ($exist != null) {
             DB::update('update keranjang set jumlah = jumlah+1 where id = ?', [$exist[0]->id]);
             return redirect("/keranjang");
         }
-        DB::insert('insert into keranjang (id_produk, id_pembeli, jumlah) values (?, ?, 1)', [$request->input("id_produk"),$request->input("id_pembeli")]);
-        
+        DB::insert('insert into keranjang (id_produk, id_pembeli, jumlah) values (?, ?, 1)', [$request->input("id_produk"), $request->input("id_pembeli")]);
+
         return redirect("/keranjang");
     }
 
     public function edit_keranjang(Request $request)
     {
-        DB::update('update keranjang set jumlah = ? where id = ?', [$request->input("jumlah"),$request->input("id_keranjang")]);
+        DB::update('update keranjang set jumlah = ? where id = ?', [$request->input("jumlah"), $request->input("id_keranjang")]);
+
         return redirect("/keranjang");
     }
 
     public function tambah_produk(Request $request)
     {
         $file = $request->file('gambar');
-        $file->move(public_path()."/foto/", $file->getClientOriginalName());
-        DB::insert("insert into parkiran (nama_parkiran,harga,kategori,gambar,detail_parkiran) values (?, ?, ?, ?, ?)", 
-        [$request->input("nama_parkiran"),$request->input("harga"),$request->input("kategori"),"foto/".$file->getClientOriginalName(),$request->input("detail_parkiran")]);
+        $file->move(public_path() . "/foto/", $file->getClientOriginalName());
+        DB::insert(
+            "insert into parkiran (nama_parkiran,harga,kategori,gambar,detail_parkiran) values (?, ?, ?, ?, ?)",
+            [$request->input("nama_parkiran"), $request->input("harga"), $request->input("kategori"), "foto/" . $file->getClientOriginalName(), $request->input("detail_parkiran")]
+        );
+
         return redirect("/tambah_produk");
     }
 
@@ -101,14 +172,19 @@ class MyController extends Controller
     {
         $file = $request->file('gambar');
         if ($file != null) {
-            $file->move(public_path()."/foto/", $file->getClientOriginalName());
-            DB::update('update parkiran set nama_parkiran = ?, harga = ?, kategori = ?, gambar = ?, detail_parkiran = ? where id = ?', 
-            [$request->input("nama_parkiran"),$request->input("harga"),$request->input("kategori"),"foto/".$file->getClientOriginalName(),$request->input("detail"),$request->input("produk_id")]);
-            return redirect("/hapusedit_produk");
+            $file->move(public_path() . "/foto/", $file->getClientOriginalName());
+            DB::update(
+                'update parkiran set nama_parkiran = ?, harga = ?, kategori = ?, gambar = ?, detail_parkiran = ? where id = ?',
+                [$request->input("nama_parkiran"), $request->input("harga"), $request->input("kategori"), "foto/" . $file->getClientOriginalName(), $request->input("detail"), $request->input("produk_id")]
+            );
+            return redirect("/hapus_edit_produk");
         }
-        DB::update('update parkiran set nama_parkiran = ?, harga = ?, kategori = ?, gambar = ?, detail_parkiran = ? where id = ?', 
-        [$request->input("nama_parkiran"),$request->input("harga"),$request->input("kategori"),$request->input('gambar_old'),$request->input("detail_parkiran"),$request->input("produk_id")]);
-        return redirect("/hapusedit_produk");
+        DB::update(
+            'update parkiran set nama_parkiran = ?, harga = ?, kategori = ?, gambar = ?, detail_parkiran = ? where id = ?',
+            [$request->input("nama_parkiran"), $request->input("harga"), $request->input("kategori"), $request->input('gambar_old'), $request->input("detail_parkiran"), $request->input("produk_id")]
+        );
+
+        return redirect("/hapus_edit_produk");
     }
 
     public function kirim_bukti_pembayaran(Request $request)
@@ -116,47 +192,126 @@ class MyController extends Controller
         $file = $request->file('gambar');
         $gambar = "";
         if ($file != null) {
-            $file->move(public_path()."/foto/", $file->getClientOriginalName());
-            $gambar = "/foto/".$file->getClientOriginalName();
+            $file->move(public_path() . "/foto/", $file->getClientOriginalName());
+            $gambar = "/foto/" . $file->getClientOriginalName();
         }
         $id_akun = $request->input("id_akun");
         $id_produk = $request->input("id_produk");
         $metode = $request->input("metode");
         DB::insert("insert into bukti_pembayaran (metode,gambar,id_akun,id_produk) values 
-        (?,?,?,?)",[$metode,$gambar,$id_akun,$id_produk]);
-		
+        (?,?,?,?)", [$metode, $gambar, $id_akun, $id_produk]);
+
         return redirect("/");
     }
-	
-	public function teracc(Request $request)
+
+    public function teracc(Request $request)
     {
-       
+
         $id_akun = $request->input("id_akun");
         $id_produk = $request->input("id_produk");
-        
-		 DB::update('update bukti_pembayaran set status = ?  where id = ?', 
-        [$request->input("status"),$request->input("id")]);
+
+        DB::update(
+            'update bukti_pembayaran set status = ?  where id = ?',
+            [$request->input("status"), $request->input("id")]
+        );
         DB::insert("insert into teracc (id_akun,id_produk) values 
-        (?,?)",[$id_akun,$id_produk]);
-		
-		
+        (?,?)", [$id_akun, $id_produk]);
+
         return redirect("/");
     }
 
     public function cari(Request $request)
     {
         $kategori = $request->get("kategori");
-        switch ($request->get("forma"))
-        {
+        switch ($request->get("forma")) {
             case "barat":
-                return view("barat",["kategori"=>$kategori]);
+                return view("barat", ["kategori" => $kategori]);
                 break;
             case "utara":
-                return view("utara",["kategori"=>$kategori]);
+                return view("utara", ["kategori" => $kategori]);
                 break;
             case "selatan":
-                return view("selatan",["kategori"=>$kategori]);
+                return view("selatan", ["kategori" => $kategori]);
                 break;
         }
+    }
+
+    public function edit_keranjang_process(Request $request, $id_keranjang)
+    {
+        $keranj = DB::select('select * from keranjang where id = ?', [$id_keranjang]);
+        $namaproduk = DB::select("select nama_parkiran from parkiran where id=?", [$keranj[0]->id_produk]);
+        $jumlah = $keranj[0]->jumlah;
+
+        return view('edit_keranjang', ["namaproduk" => $namaproduk[0]->nama_parkiran, "jumlah" => $jumlah, "id_keranjang" => $id_keranjang]);
+    }
+
+    public function hapus_keranjang_process(Request $request, $id_keranjang)
+    {
+        DB::delete("delete from keranjang where id = ?", [$id_keranjang]);
+
+        return redirect("/keranjang");
+    }
+
+    public function checkout(Request $request, $id_keranjang)
+    {
+        $keranj = DB::select('select * from keranjang where id = ?', [$id_keranjang]);
+        $namaproduk = DB::select("select * from parkiran where id=?", [$keranj[0]->id_produk]);
+        $jumlah = $keranj[0]->jumlah;
+        DB::delete("delete from keranjang where id = ?", [$id_keranjang]);
+
+        return view('checkout', ["produk" => $namaproduk[0], "jumlah" => $jumlah, "keranj" => $keranj[0]]);
+    }
+
+    public function teracc_process(Request $request, $id_keranjang)
+    {
+        DB::insert("insert into teracc (id_akun,id_produk) values (?,?)", [$id_akun, $id_produk]); // ANOMALI
+        DB::delete("delete from bukti_pembayaran where id = ?", [$id_keranjang]);
+        $bukti_pembayaran = DB::select("select * from bukti_pembayaran");
+
+
+        return view("lihat_bukti_pembayaran", ["bukti_pembayaran" => $bukti_pembayaran]);
+    }
+
+    public function dteracc_process(Request $request, $id_keranjang)
+    {
+        DB::delete("delete from bukti_pembayaran where id = ?", [$id_keranjang]);
+        $bukti_pembayaran = DB::select("select * from bukti_pembayaran");
+
+        return view("lihat_bukti_pembayaran", ["bukti_pembayaran" => $bukti_pembayaran]);
+    }
+
+    public function hapus(Request $request, $id_produk)
+    {
+        DB::delete("delete from parkiran where id = ?", [$id_produk]);
+
+        return redirect("/hapusedit_produk");
+    }
+
+    public function edit(Request $request, $id_produk)
+    {
+        $prod = DB::select("select * from parkiran where id = ?", [$id_produk]);
+
+        return view("edit_produk", ["produk" => $prod[0]]);
+    }
+
+    public function lihat_bukti_pembayaran(Request $request)
+    {
+        $bukti_pembayaran = DB::select("select * from bukti_pembayaran");
+
+        return view("lihat_bukti_pembayaran", ["bukti_pembayaran" => $bukti_pembayaran]);
+    }
+
+    public function lihat_bukti_teracc(Request $request)
+    {
+        $user = DB::select("select * from akun where csrf = ?", [csrf_token()]);
+        $bukti_pembayaran = DB::select("select * from teracc where id_akun = ?", [$user[0]->id]);
+
+        return view("lihat_bukti_teracc", ["bukti_pembayaran" => $bukti_pembayaran]);
+    }
+
+    public function logout(Request $request)
+    {
+        session()->regenerate();
+        return redirect("login");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,110 +15,77 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
-Route::get('/', function () {
-    return view('landing');
-});
+// Landing
+Route::get('/',[MyController::class, 'landing']);
 
 
-Route::get('/about_us', function () {
-    return view('about_us');
-});
-Route::get('/login', function () {
-    return view('login');
-});
+// Login - Register - Logout
+Route::get('/login',[MyController::class, 'login_view']);
+Route::get('/register', [MyController::class,"register_view"]);
 Route::get('/logout', [MyController::class,"logout"]);
-Route::get('/register', function () {
-    return view('register');
-});
-Route::get('/keranjang', function () {
-    return view('keranjang');
-});
-Route::get('/barat', function () {
-    return view('barat');
-});
-Route::get('/utara', function () {
-    return view('utara');
-});
-Route::get('/selatan', function () {
-    return view('selatan');
-});
-Route::get('/about_us', function () {
-    return view('about_us');
-});
-Route::get('/profile', function () {
-    return view('profile');
-});
-Route::get('/admin_reg', function () {
-    return view('register_admin');
-});
-Route::get('/tambah_produk', function () {
-    return view('tambah_produk');
-});
-Route::get('/hapusedit_produk', function () {
-    return view('hapusedit_produk');
-});
-Route::get('/edit_keranjang/{id_keranjang}', function ($id_keranjang) {
-    $keranj = DB::select('select * from keranjang where id = ?', [$id_keranjang]);
-    $namaproduk = DB::select("select nama_parkiran from parkiran where id=?", [$keranj[0]->id_produk]);
-    $jumlah = $keranj[0]->jumlah;
-    return view('edit_keranjang',["namaproduk"=>$namaproduk[0]->nama_parkiran,"jumlah"=>$jumlah,"id_keranjang"=>$id_keranjang]);
-});
-Route::get('/hapus_keranjang/{id_keranjang}', function ($id_keranjang) {
-    DB::delete("delete from keranjang where id = ?", [$id_keranjang]);
-    return redirect("/keranjang");
-});
-Route::get('/checkout/{id_keranjang}', function ($id_keranjang) {
-    $keranj = DB::select('select * from keranjang where id = ?', [$id_keranjang]);
-    $namaproduk = DB::select("select * from parkiran where id=?", [$keranj[0]->id_produk]);
-    $jumlah = $keranj[0]->jumlah;  
-	DB::delete("delete from keranjang where id = ?", [$id_keranjang]);	
-    return view('checkout',["produk"=>$namaproduk[0],"jumlah"=>$jumlah,"keranj"=>$keranj[0]]);
-});
-Route::get('/teracc/{id_keranjang}', function ($id_keranjang) {
-	
-        
-        DB::insert("insert into teracc (id_akun,id_produk) values 
-        (?,?)",[$id_akun,$id_produk]);
-	DB::delete("delete from bukti_pembayaran where id = ?", [$id_keranjang]);
-	$bukti_pembayaran = DB::select("select * from bukti_pembayaran");
-	
-		
-    return view("lihat_bukti_pembayaran",["bukti_pembayaran"=>$bukti_pembayaran]);
-});
-Route::get('/dteracc/{id_keranjang}', function ($id_keranjang) {
-	DB::delete("delete from bukti_pembayaran where id = ?", [$id_keranjang]);
-	$bukti_pembayaran = DB::select("select * from bukti_pembayaran");
-    return view("lihat_bukti_pembayaran",["bukti_pembayaran"=>$bukti_pembayaran]);
-});
-Route::get('/produk/hapus/{id_produk}', function ($id_produk) {
-    DB::delete("delete from parkiran where id = ?", [$id_produk]);
-    return redirect("/hapusedit_produk");
-});
-Route::get('/produk/edit/{id_produk}', function ($id_produk) {
-    $prod = DB::select("select * from parkiran where id = ?",[$id_produk]);
-    return view("edit_produk",["produk"=>$prod[0]]);
-});
-Route::get('/lihat_bukti_pembayaran', function () {
-    $bukti_pembayaran = DB::select("select * from bukti_pembayaran");
-    return view("lihat_bukti_pembayaran",["bukti_pembayaran"=>$bukti_pembayaran]);
-});
-Route::get('/lihat_bukti_teracc', function () {
-	$user = DB::select("select * from akun where csrf = ?", [csrf_token()]);
-	$bukti_pembayaran = DB::select("select * from teracc where id_akun = ?", [$user[0]->id]);
-    return view("lihat_bukti_teracc",["bukti_pembayaran"=>$bukti_pembayaran]);
-});
-Route::post('/edit_profile',[MyController::class, 'edit_profile']);
-Route::post('/register_acc',[MyController::class, 'register']);
-Route::post('/produk/tambah',[MyController::class, 'tambah_produk']);
-Route::post('/produk/edit/confirm',[MyController::class, 'edit_produk']);
-Route::post('/register_acc_admin',[MyController::class, 'register_admin']);
-Route::post('/login_acc',[MyController::class, 'login']);
-Route::post('/beli',[MyController::class, 'beli']);
-Route::post('/edit/keranjang',[MyController::class, 'edit_keranjang']);
-Route::post('/kirim_bukti_pembayaran',[MyController::class, 'kirim_bukti_pembayaran']);
-Route::post('/teracc',[MyController::class, 'teracc']);
-Route::get('/search', function () {
-    return view('search');
-});
 
+Route::post('/login_acc',[MyController::class, 'login']);
+Route::post('/register_acc',[MyController::class, 'register']);
+
+
+// About Us
+Route::get('/about_us',[MyController::class, 'about_us']);
+
+
+// Profile
+Route::get('/profile', [MyController::class,"profile"]);
+
+Route::post('/edit_profile',[MyController::class, 'edit_profile']);
+
+
+// Product
+Route::get('/tambah_produk', [MyController::class,"tambah_produk_view"]);
+Route::get('/hapus_edit_produk', [MyController::class,"hapus_edit_produk"]);
+
+Route::post('/produk/tambah',[MyController::class, 'tambah_produk']);
+Route::get('/produk/hapus/{id_produk}', [MyController::class,"hapus"]);
+Route::get('/produk/edit/{id_produk}', [MyController::class,"edit"]);
+Route::post('/produk/edit/confirm',[MyController::class, 'edit_produk']);
+
+
+// Find - Cari
 Route::get("/cari",[MyController::class,'cari'])->name("cari");
+Route::get('/search', [MyController::class,"search"]);
+
+
+// Buy
+Route::post('/beli',[MyController::class, 'beli']);
+
+
+// Cart
+Route::get('/keranjang', [MyController::class,"keranjang"]);
+Route::get('/edit_keranjang/{id_keranjang}', [MyController::class,"edit_keranjang_process"]);
+Route::get('/hapus_keranjang/{id_keranjang}', [MyController::class,"hapus_keranjang_process"]);
+
+Route::post('/edit/keranjang',[MyController::class, 'edit_keranjang']);
+
+
+// Category ( Barat - Utara - Selatan )
+Route::get('/barat', [MyController::class,"barat"]);
+Route::get('/utara', [MyController::class,"utara"]);
+Route::get('/selatan', [MyController::class,"selatan"]);
+
+
+// Admin
+Route::get('/admin_reg', [MyController::class,"admin_reg"]);
+
+Route::post('/register_acc_admin',[MyController::class, 'register_admin']);
+
+// Confirmation
+Route::get('/checkout/{id_keranjang}', [MyController::class,"checkout"]);
+Route::get('/teracc/{id_keranjang}', [MyController::class,"teracc_process"]);
+Route::get('/dteracc/{id_keranjang}', [MyController::class,"dteracc_process"]);
+
+Route::post('/teracc',[MyController::class, 'teracc']);
+
+
+// Lainnya
+Route::get('/lihat_bukti_pembayaran', [MyController::class,"lihat_bukti_pembayaran"]);
+Route::get('/lihat_bukti_teracc', [MyController::class,"lihat_bukti_teracc"]);
+
+Route::post('/kirim_bukti_pembayaran',[MyController::class, 'kirim_bukti_pembayaran']);
